568638072a1e259ae98672ff97bae7c068322fc7
Tuaannn <vantuan.97.pt@gmail.com>
2024-06-02 11:51:52 +0700

    ...

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // const deg = 6;
-// const hr = document.querySelector("#hr");
-// const mn = document.querySelector("#mn");
-// const sc = document.querySelector("#sc");
+// const hr = document.querySelector(".hr");
+// const mn = document.querySelector(".mn");
+// const sc = document.querySelector(".sc");
 
 // setInterval(() => {
 //     let date = new Date();
@@ -9,25 +9,31 @@
 //     let hh = date.getHours() * 30;
 //     let mm = date.getMinutes() * deg;
 //     let ss = date.getSeconds() * deg;
-//     console.log(hh, mm);
-//     hr.style.transform = `rotate(${hh + mm / 12}deg)`;
-//     mn.style.transform = `rotate(${mm}deg)`;
-//     sc.style.transform = `rotate(${ss}deg)`;
-// });
 
-const deg = 6;
-const hr = document.querySelector(".hr");
-const mn = document.querySelector(".mn");
-const sc = document.querySelector(".sc");
+//     hr.style.transform = `rotate(${hh + mm / 12}deg)`;
+//     mn.style.transform = `rotate(${mm + ss / 60}deg)`;
+//     sc.style.transform = `rotate(${ss}deg)`;
+// }, 1000);
+
+let hr = document.querySelector(".hr");
+let mn = document.querySelector(".mn");
+let sc = document.querySelector(".sc");
+let tickSound = document.querySelector(".tickSound");
 
 setInterval(() => {
     let date = new Date();
 
     let hh = date.getHours() * 30;
-    let mm = date.getMinutes() * deg;
-    let ss = date.getSeconds() * deg;
+    let mm = date.getMinutes() * 6;
+    let ss = date.getSeconds() * 6;
+    let miniSecond = date.getMilliseconds() * 0.006;
 
     hr.style.transform = `rotate(${hh + mm / 12}deg)`;
     mn.style.transform = `rotate(${mm + ss / 60}deg)`;
     sc.style.transform = `rotate(${ss}deg)`;
-}, 1000);
+
+    if (date.getMilliseconds() < 30) {
+        tickSound.currentTime = 0;
+        tickSound.play();
+    }
+});
